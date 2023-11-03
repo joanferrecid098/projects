15305673efc2a7550916fc36e5ce8fe0aadfeb2f
joanferrecid098 <joanferrecid@gmail.com>
2023-11-03 18:27:16 +0100

    wa-auto

---
+++ wa-auto/main.ts
@@ -0,0 +1,23 @@
+import qrcode from 'qrcode-terminal';
+import { Client, LocalAuth } from 'whatsapp-web.js';
+
+const client = new Client({
+    authStrategy: new LocalAuth(),
+    puppeteer: { 
+        headless: true
+    }
+});
+
+client.on('qr', qr => {
+    qrcode.generate(qr, {small: true});
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.on('message', message => {
+	message.reply(message.body);
+});
+
+client.initialize();
